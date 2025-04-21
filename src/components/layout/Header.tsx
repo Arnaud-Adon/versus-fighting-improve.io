45269f528c265d1191aff7138a986c89148305db
Arnaud Adon <assy.adon@yahoo.fr>
2025-04-21 15:47:53 +0200

    refactor: header style update

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,12 +1,11 @@
 import { Auth } from "@/features/auth/Auth";
-import Image from "next/image";
 import Link from "next/link";
 
 export function Header() {
   return (
-    <div className="flex items-center">
+    <div className="flex items-center border-b border-zinc-400 py-4 px-8">
       <Link href="/">
-        <Image src="/next.svg" alt="logo" width={100} height={100} />
+        <h1 className="text-2xl font-bold">VSF.io</h1>
       </Link>
       <div className="ml-auto">
         <Auth />
